e2a40b6b4e595854fa9df26538c835407ef7e503
Nikodimos Mekonen <nikodimosm.01@gmail.com>
2025-03-17 19:54:54 +0300

    added the edit finctionality

--- ToDo/main.js
+++ ToDo/main.js
@@ -18,16 +18,26 @@ window.addEventListener('load', function() {
     const input = document.getElementById("task_input");
     const button = document.getElementById("add");
     const taskDiv = document.getElementById("taskDiv");
-    const tasks = []; // Initialize tasks array
+    const tasks = new Map(); // Initialize tasks array
   
     button.addEventListener("click", function() {
       const taskText = input.value.trim(); // Trim whitespace
       if (taskText !== "") { // Prevent adding empty tasks
-          tasks.push(taskText);
+          const id= new Date().toISOString();
+          tasks.set(id ,taskText);
           const newParagraph = document.createElement("p"); // Create <p> element
-          newParagraph.textContent = taskText; // Set its content (more efficient)
+          newParagraph.textContent = tasks.get(id); // Set its content (more efficient)
+          newParagraph.setAttribute("class","tasks")
+          newParagraph.setAttribute("onclick","edit_task(this)")
+          newParagraph.setAttribute("id",id)
           taskDiv.appendChild(newParagraph); // Append <p> to taskDiv
           input.value = ""; // Clear the input field
       }
     });
+    
   });
+  function edit_task(element){
+    const input = prompt("EDIT TASK",element.textContent)
+    element.innerHTML=input
+    
+  }
