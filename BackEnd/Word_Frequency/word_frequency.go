e0be8be0f801239b97bcd51127e84d85fd41e2a8
Nikodimos Mekonen <nikodimosm.01@gmail.com>
2025-04-05 11:30:35 +0300

    finetuned the filteration for alphabets

--- BackEnd/Word_Frequency/word_frequency.go
+++ BackEnd/Word_Frequency/word_frequency.go
@@ -10,19 +10,20 @@ func main() {
 	var word string
 	m := make(map[string]uint)
 	input := bufio.NewReader(os.Stdin)
-	fmt.Print("Please enter a string\n")
+	fmt.Print("Please enter a string: \n")
 	word, _ = input.ReadString('\n')
 	if(len(word) != 1){
 		for k:=0;k<len(word);k++{
 			_, found := m[string(word[k])]
 			if(found){
 				m[string(word[k])]+=1
-			}else{
-				m[string(word[k])]=1
+			}else if word[k] >= 65 && word[k]<= 122{
+				if word[k] > 96 || word[k]< 91 {
+					m[string(word[k])]=1
+				}
 			}
 		}
 	}else{
-		fmt.Print("Please enter a string\n")
 		main()
 	}
 	fmt.Println(m)
