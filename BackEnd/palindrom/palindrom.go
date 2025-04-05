a39e681c2b2896f7c0190cefc3363fc0da0f61ae
Nikodimos Mekonen <nikodimosm.01@gmail.com>
2025-04-05 21:05:29 +0300

    fixed some test cases but works for most

--- BackEnd/palindrom/palindrom.go
+++ BackEnd/palindrom/palindrom.go
@@ -11,17 +11,31 @@ func main() {
 	var word string
 	input := bufio.NewReader(os.Stdin)
 	fmt.Print("Please enter a string: \n")
+	alphanumeric:="1234567890qwertyuiopasdfghjklzxcvbnm"
 	word, _ = input.ReadString('\n')
-	word=strings.ToUpper(string(word))
+	word = strings.Trim(word,"\n")
+	word=strings.ToLower(string(word))
 	count := len(word)-1
 	for i:=0;i<=len(word)/2;i++{
-		if word[i] >= 65 && word[i]<= 190{
-			if(word[i]==word[count] ){
+		if strings.Contains(alphanumeric, string(word[i])) && strings.Contains(alphanumeric, string(word[count])) {
+			if word[i] == word[count]{
+				// fmt.Printf("equal %v\n",string(word[count]))
 				count-=1
-			}else if word[i]!=word[count]{
+			}else{
 				fmt.Println("not a palindrome!")
 				return
 			}
+
+		}else if strings.Contains(alphanumeric, string(word[i])) {
+			// fmt.Printf("i %v\t %v\n",string(word[count]),count)
+			count-=1
+			i-=1
+		}
+		// fmt.Printf("the i %v\n",i)
+		if count==i{
+			break
 		}
 	}
+	fmt.Println("palindrome!")
+	return
 }
