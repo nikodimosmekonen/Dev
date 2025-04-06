4b31e3a315131030109c11753c15cc9b0d8734ec
Nikodimos Mekonen <nikodimosm.01@gmail.com>
2025-04-06 17:55:16 +0300

    finished the interface implementation and added AddMember

--- BackEnd/library_management/services/library_service.go
+++ BackEnd/library_management/services/library_service.go
@@ -40,6 +40,48 @@ func (l Library) BorrowBook(memberID int, bookID int) error {
 	if !exists {
 		return errors.New("member doesn't exist")
 	}
+	book.Status = "Borrowed"
+	l.books[bookID] = book
+
 	member.BorrowedBooks = append(member.BorrowedBooks, book)
+	l.members[memberID] = member
 	return nil
+}
+func (l Library) ReturnBook(bookID int, memberID int) error {
+	book, exists := l.books[bookID]
+	if !exists {
+		return errors.New("book doesn't exist")
+	}
+	member, exists := l.members[memberID]
+	if !exists {
+		return errors.New("member doesn't exist")
+	}
+	book.Status = "Available"
+	l.books[bookID] = book
+
+	member.BorrowedBooks = member.BorrowedBooks[:len(member.BorrowedBooks)-2]
+	l.members[memberID] = member
+	return nil
+}
+func (l Library) ListAvailableBooks() []models.Book{
+	books := []models.Book{}
+	for _,v := range l.books {
+		if v.Status == "Available"{
+			books=append(books,v)
+		}
+	}
+
+	return books
+}
+func (l Library)ListBorrowedBooks(memberID int) []models.Book{
+	books := []models.Book{}
+	for _,v := range l.books {
+		if v.Status == "Borrowed"{
+			books=append(books,v)
+		}
+	}
+	return books
+}
+func (l Library) AddMember(member models.Member) {
+	l.members[member.ID] = member
 }
